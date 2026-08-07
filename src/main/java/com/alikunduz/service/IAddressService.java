aabfceaedb7c2ff82826f78f9b7b0e509a4d79f9
Muhammet Ali Kunduz <alikunduz38@gmail.com>
2026-08-08 02:52:50 +0300

    UPDATED

--- src/main/java/com/alikunduz/service/IAddressService.java
+++ src/main/java/com/alikunduz/service/IAddressService.java
@@ -1,4 +1,9 @@
 package com.alikunduz.service;
 
+import com.alikunduz.dto.DtoAddress;
+import com.alikunduz.dto.DtoAddressIU;
+
 public interface IAddressService {
+
+    public DtoAddress saveAddress(DtoAddressIU  dtoAddressIU);
 }
--- src/main/java/com/alikunduz/service/impl/AddressServiceImpl.java
+++ src/main/java/com/alikunduz/service/impl/AddressServiceImpl.java
@@ -1,10 +1,38 @@
 package com.alikunduz.service.impl;
 
+import com.alikunduz.dto.DtoAddress;
+import com.alikunduz.dto.DtoAddressIU;
+import com.alikunduz.model.Address;
+import com.alikunduz.repository.AddressRepository;
 import com.alikunduz.service.IAddressService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class AddressServiceImpl implements IAddressService {
 
+    @Autowired
+    private AddressRepository addressRepository;
 
+
+    private Address createAddress(DtoAddressIU dtoAddressIU) {
+        Address address = new Address();
+        address.setCreateTime(new Date());
+
+        BeanUtils.copyProperties(dtoAddressIU, address);
+        return address;
+    }
+
+
+    @Override
+    public DtoAddress saveAddress(DtoAddressIU dtoAddressIU) {
+        DtoAddress dtoAddress = new DtoAddress();
+        Address savedAddress = addressRepository.save(createAddress(dtoAddressIU));
+
+        BeanUtils.copyProperties(savedAddress, dtoAddress);
+        return dtoAddress;
+    }
 }
